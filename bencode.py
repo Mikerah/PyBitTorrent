bbc0c1b9ff27eeef19240f20ad07a2e5e997db9c
Mikerah <mikerah14@gmail.com>
2017-10-30 21:43:01 -0400

    Added skeleton code to bencode.py

---
+++ bencode.py
@@ -0,0 +1,17 @@
+import re
+
+def encode(to_encode):
+    """
+    Encodes the given string into Bencoding format
+    :param to_encode
+    returns str
+    """
+    pass
+    
+def decode(to_decode):
+    """
+    Decodes the given string into its correct type
+    :param to_decode
+    returns int, string, list, dictionary
+    """
+    pass
