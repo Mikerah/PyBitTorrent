bbea65471d3753586e9c0dd0eb29c7882c552ef6
Mikerah <mikerah14@gmail.com>
2017-10-31 16:18:11 -0400

    Added skeleton code for test cases for decoding bencoded strings

--- tests/bencode_tests.py
+++ tests/bencode_tests.py
@@ -1,1 +1,15 @@
-C:\Users\Mikerah\Documents\GitHub\PyBitTorrent\tests
+import unittest
+
+class TestDecodingForBencode(unittest.Testcase):
+    
+    def test_str(self):
+        pass
+    
+    def test_int(self):
+        pass
+        
+    def test_list(self):
+        pass
+        
+    def test_dictionary(self):
+        pass
